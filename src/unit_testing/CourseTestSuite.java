6959c9021df4d65f2d49d3754225069a794cd167
Adam Kenny <adam@adamkenny.net>
2014-04-20 12:23:48 -0400

    skeleton classes for all 3 test suites

---
+++ src/unit_testing/CourseTestSuite.java
@@ -0,0 +1,24 @@
+/**
+ * Package used to test Mini-Golf Course
+ */
+package unit_testing;
+
+/**
+ * @author Adam Kenny
+ * @author Dang Yang
+ * @author Lloyd Pintac
+ * 
+ * Test suite for Course class
+ *
+ */
+public class CourseTestSuite {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/unit_testing/MainTestSuite.java
@@ -0,0 +1,24 @@
+/**
+ * Package used to test Mini-Golf Course
+ */
+package unit_testing;
+
+/**
+ * @author Adam Kenny
+ * @author Dang Yang
+ * @author Lloyd Pintac
+ * 
+ * Test suite for Main class
+ *
+ */
+public class MainTestSuite {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
